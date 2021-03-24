d5e84690fa90e346818789d6b5f130e1f1bcf8dc
anaik91 <ashwin@kamivision.com>
2021-03-25 01:41:56 +0530

    changes

--- app.py
+++ app.py
@@ -1,17 +1,46 @@
 import os
-from flask import Flask
+from flask import Flask,jsonify,request,redirect, url_for
 
 
 app = Flask(__name__)
+app.config['DUMP_LOCATION'] = os.getenv('DUMP_LOCATION')
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+def write2file(file,data):
+    with open(file,'w') as fl:
+        fl.write(data)
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/fetch/<file>', methods = ['GET'])
+def fetch(file):
+    filename='{}/{}'.format(app.config.get('DUMP_LOCATION'),file)
+    data = open(filename).read()
+    return '''
+            <div>
+            <p name="data">{}</textarea>
+            </div>
+            '''.format(data)
+
+@app.route('/dump', methods = ['GET', 'POST'])
+def dump():
+    if request.method == 'POST':
+        file = request.form.get('file')
+        data = request.form.get('data')
+        filename='{}/{}'.format(app.config.get('DUMP_LOCATION'),file)
+        write2file(filename,data)
+        print(url_for('fetch',file=file))
+        return redirect(url_for('fetch', file=file))
+    return '''
+            <form method="POST">
+                <div><label>File Name: <input type="text" name="file"></label></div>
+                <div>
+                <textarea name="data" rows="4" cols="50">
+                Enter Something..</textarea>
+                </div>
+                <input type="submit" value="Submit">
+            </form>'''
 
 @app.route('/healthz')
 def healthz():
