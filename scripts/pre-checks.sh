401aeab386c5270193e64e877f99b5c1436760d9
anaik91 <ashwin@kamivision.com>
2021-03-27 19:23:00 +0530

    modified pre post script

--- scripts/pre-checks.sh
+++ scripts/pre-checks.sh
@@ -5,7 +5,7 @@ echo "Checking if Docker is UP"
 docker info --format '{{json .ServerVersion}}'
 
 echo "Checking if K8s is UP"
-kubectl cluster-info
+kubectl --kubeconfig ${CONFIG} cluster-info
 
 echo "Resource Status in ${ENV}"
 kubectl get --kubeconfig ${CONFIG} deploy,svc,pv,pvc,configmap  -n ${ENV}
