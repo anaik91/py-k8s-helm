ddf2e8905db2ae70f4dee9b49c186c3dc825a517
anaik91 <ashwin@kamivision.com>
2021-03-27 17:48:21 +0530

    refactor

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
+"""
+Sample Wiki App using Flask
+"""
 import os
-from flask import Flask,jsonify,request,redirect, url_for
+from flask import Flask,request,redirect, url_for
 
 
 app = Flask(__name__)
@@ -7,14 +10,23 @@ app.config['DUMP_LOCATION'] = os.getenv('DUMP_LOCATION')
 
 @app.route('/')
 def hello():
+    """
+    Default Index
+    """
     return "Hello World! v2"
 
 def write2file(file,data):
-    with open(file,'w') as fl:
-        fl.write(data)
+    """
+    Function to write to file
+    """
+    with open(file,'w') as filehandler:
+        filehandler.write(data)
 
 @app.route('/fetch/<file>', methods = ['GET'])
 def fetch(file):
+    """
+    Route to Render files from DUMP_LOCATION
+    """
     filename='{}/{}'.format(app.config.get('DUMP_LOCATION'),file)
     data = open(filename).read()
     return '''
@@ -25,6 +37,9 @@ def fetch(file):
 
 @app.route('/dump', methods = ['GET', 'POST'])
 def dump():
+    """
+    Route to Dump files from DUMP_LOCATION
+    """
     if request.method == 'POST':
         file = request.form.get('file')
         data = request.form.get('data')
@@ -44,6 +59,9 @@ def dump():
 
 @app.route('/healthz')
 def healthz():
+    """
+    Route for Health Check
+    """
     return "OK"
 
 if __name__ == '__main__':
